abdb688552f9bf1b24ee46b8589f1bed82899ca1
Erox-02 <relic.of.sky@gmail.com>
2026-09-13 00:15:00 +0530

    IT WORKS

--- src/extractors/manager.rs
+++ src/extractors/manager.rs
@@ -18,9 +18,9 @@ impl Exman {
 
     pub fn add_conex(&mut self, config: Excon) -> Result<(), String> {
         let extractor = Conex::new(config)?;
-        self.enabled.insert(extractor.clone());
-        self.extractors.push(extractor);
         log::info!("registered extractor {}", extractor.name());
+        self.enabled.insert(extractor.name().to_string());
+        self.extractors.push(extractor);
         Ok(())
     }
 
